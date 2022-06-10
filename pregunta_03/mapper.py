afb653e8f5d83a0bf1ffc06644a72e48978ff1bb
fa <fa@sempli.co>
2022-06-09 23:36:43 -0500

    ready

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,9 @@
-#
-# >>> Escriba el codigo del mapper a partir de este punto <<<
-#
+
+#! /usr/bin/ python3
+
+import sys
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        a,b = line.split(',')
+        sys.stdout.write('{}\t{}\n'.format(a,b))
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,29 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+
+if __name__ == "__main__":
+
+    acc = []
+    i = 0
+    for line in sys.stdin:
+        if len(line.split('\t')) > 1:
+            a,b = line.split('\t')
+            b = int(b)
+            acc.append((a,b))
+            
+            
+def sort_tuple(tup):
+    lst = len(tup)
+    for i in range(0, lst):
+         
+        for j in range(0, lst-i-1):
+            if (tup[j][1] > tup[j + 1][1]):
+                temp = tup[j]
+                tup[j]= tup[j + 1]
+                tup[j + 1]= temp
+    return tup
+
+for a,b in sort_tuple(acc):
+    sys.stdout.write("{}\t{}\n".format(a, b))
+
+
+ 
