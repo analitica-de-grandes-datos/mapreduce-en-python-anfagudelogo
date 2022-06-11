acc3898b39faaab3364b089fc162928f7237b750
fa <fa@sempli.co>
2022-06-11 17:08:40 -0500

    listo

--- pregunta_05/reducer.py
+++ pregunta_05/reducer.py
@@ -1,3 +1,16 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+
+if __name__ == "__main__":
+
+    acc = {}
+    for line in sys.stdin:
+        if len(line.split("\t")) > 1:
+            col1, col2, col3 = line.split("\t")
+            col2 = col2[5:7]
+            if col2 not in acc.keys():
+                acc[col2] = 1
+            else:
+                acc[col2] = acc[col2] + 1
+
+    for key in sorted(acc):
+        sys.stdout.write("{}\t{}\n".format(key, acc[key]))
--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,28 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+
+if __name__ == "__main__":
+
+    acc_max = {}
+    acc_min = {}
+    for line in sys.stdin:
+        if len(line.split("\t")) > 1:
+            col1, col2, col3 = line.split("\t")
+            col3 = float(col3)
+            if col1 not in acc_max.keys():
+                acc_max[col1] = col3
+            else:
+                if acc_max[col1] > col3:
+                    acc_max[col1] = acc_max[col1]
+                else:
+                    acc_max[col1] = col3
+
+            if col1 not in acc_min.keys():
+                acc_min[col1] = col3
+            else:
+                if acc_min[col1] < col3:
+                    acc_min[col1] = acc_min[col1]
+                else:
+                    acc_min[col1] = col3
+
+    for key in acc_max.keys():
+        sys.stdout.write("{}\t{}\t{}\n".format(key, acc_max[key], acc_min[key]))
