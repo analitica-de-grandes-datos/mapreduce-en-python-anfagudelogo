8d74596c2271a605cd883e3114f5a9f32f192036
fa <fa@sempli.co>
2022-06-11 18:07:14 -0500

    listo

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == "__main__":
 
     for line in sys.stdin:
-        a, b, c = line.split("   ")
+        a, b, c = line.split()
 
         sys.stdout.write("{}\t{}\t{}\n".format(a, b, c))
 
--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,24 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+
+if __name__ == "__main__":
+
+    acc_count = {}
+    acc_sum = {}
+    for line in sys.stdin:
+        if len(line.split("\t")) > 1:
+            col1, col2, col3 = line.split("\t")
+            col3 = int(col3)
+            if col1 not in acc_count.keys():
+                acc_count[col1] = 1
+            else:
+                acc_count[col1] = acc_count[col1] + 1
+
+            if col1 not in acc_sum.keys():
+                acc_sum[col1] = col3
+            else:
+                acc_sum[col1] = acc_sum[col1] + col3
+
+    for key in acc_count.keys():
+        sys.stdout.write(
+            "{}\t{}\t{}\n".format(key, acc_sum[key], acc_sum[key] / acc_count[key])
+        )
