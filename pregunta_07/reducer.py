f59c303f1ec3157d3465bf9b405916c4d1b07a23
fa <fa@sempli.co>
2022-06-11 17:46:57 -0500

    listo

--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,17 @@
-#
-# >>> Escriba el codigo del reducer a partir de este punto <<<
-#
+import sys
+
+if __name__ == "__main__":
+
+    acc = []
+    for line in sys.stdin:
+        if len(line.split()) > 1:
+            a, b, c = line.split()
+            c = int(c)
+            acc.append((a, b, c))
+        else:
+            pass
+
+    for col1, col2, col3 in sorted(acc, key=lambda element: (element[0], element[2])):
+        sys.stdout.write("{}\t{}\t{}\n".format(col1, col2, col3))
+
+# sorted(unsorted, key=lambda element: (element[1], element[2]))
